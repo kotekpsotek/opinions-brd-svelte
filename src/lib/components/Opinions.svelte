ed5ea6b87514346058f0444055dc1efacedaffdb
kotekpsotek <siematumichael@protonmail.com>
2023-11-26 22:02:13 +0100

    Write opinion section must be expanded toward use to share opinion, attched close button as the amendments to close expanded share opinion section

--- src/lib/components/Opinions.svelte
+++ src/lib/components/Opinions.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-    import { Input, Label, Rating } from "flowbite-svelte";
+    import { Rating } from "flowbite-svelte";
     // import InputIcon from "./utils/InputIcon.svelte"
+    import { Forum, CloseOutline } from "carbon-icons-svelte";
 
     export let starsCount: StarsCount;
+    export let shareOpinionIconSize = 25 as 32;
 
     let email = "";
     let userName = "";
@@ -10,38 +12,56 @@
     let content = "";
 
     let rating: number = 1
+    let expandWriteOpinion = false;
+
+    function fnExpandCloseWriteOpinion() {
+        expandWriteOpinion = !expandWriteOpinion;
+    }
 </script>
 
 <section id="opinions-layout">
     <div id="existsing" class="flex gap-x-2">
         
     </div>
-    <div id="write-new" class="flex flex-col gap-y-4">
-        <div id="one-schema">
-            <h2 class="h2">Your estimmate</h2>
-            <div class="flex justify-center gap-x-5">
-                <input type="number" class="input" min=1 max={starsCount} step=0.1 bind:value={rating} placeholder="Your evaluation">
-                {#key rating}
-                    <Rating size={40} rating={rating}/>
-                {/key}
-            </div>
-        </div>
-        <div id="inputs" class="flex flex-col gap-y-2 w-full">
+    <!-- Open write new opinion menu -->
+    {#if expandWriteOpinion}
+        <div id="write-new" class="flex flex-col gap-y-4">
             <div id="one-schema">
-                <h2 class="h2">How other see you</h2>
-                <div class="flex gap-x-2 w-full">
-                    <input type="text" class="input w-1/2" placeholder="Email">
-                    <input type="text" class="input w-1/2" placeholder="Your Name" bind:value={userName}>
-                </div>
-                <!-- <InputIcon bind:value={email} placeholder="Email"/> -->
-            </div>
-            <div id="one-schema">
-                <h2 class="h2">Your Opinion</h2>
-                <div class="flex flex-col gap-y-2">
-                    <input class="input" type="text" placeholder="Title" bind:value={title}>
-                    <textarea class="input" placeholder="Content" id="" cols="30" rows="10" bind:value={content}></textarea>
+                <h2 class="h2">Your estimmate</h2>
+                <div class="flex justify-center gap-x-5">
+                    <input type="number" class="input" min=1 max={starsCount} step=0.1 bind:value={rating} placeholder="Your evaluation">
+                    {#key rating}
+                        <Rating size={40} rating={rating}/>
+                    {/key}
                 </div>
             </div>
+            <div id="inputs" class="flex flex-col gap-y-2 w-full">
+                <div id="one-schema">
+                    <h2 class="h2">How other see you</h2>
+                    <div class="flex gap-x-2 w-full">
+                        <input type="text" class="input w-1/2" placeholder="Email">
+                        <input type="text" class="input w-1/2" placeholder="Your Name" bind:value={userName}>
+                    </div>
+                    <!-- <InputIcon bind:value={email} placeholder="Email"/> -->
+                </div>
+                <div id="one-schema">
+                    <h2 class="h2">Your Opinion</h2>
+                    <div class="flex flex-col gap-y-2">
+                        <input class="input" type="text" placeholder="Title" bind:value={title}>
+                        <textarea class="input" placeholder="Content" id="" cols="30" rows="10" bind:value={content}></textarea>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+        <div id="actions" class="flex justify-end mt-2">
+            <button id="close" class="p-5 bg-slate-500 rounded-xl" on:click={fnExpandCloseWriteOpinion}>
+                <CloseOutline size={24} fill="white"/>
+            </button>
+        </div>
+    {:else}
+        <button id="write-new-ex" class="w-full pl-2 flex justify-start items-center gap-x-5 border-solid border input border-slate-500 cursor-pointer transition-all hover:scale-95 hover:shadow-lg hover:shadow-slate-950" on:click={fnExpandCloseWriteOpinion}>
+            <Forum size={shareOpinionIconSize} fill="rgb(105, 109, 113)"/>
+            <input type="text" class="border-none p-none m-none" placeholder="Share your opinion..." disabled>
+        </button>
+    {/if}
 </section>
