8addbd55bf9348a40f88dbec0103aee8c06dd781
kotekpsotek <siematumichael@protonmail.com>
2023-11-30 15:43:04 +0100

    From pagination user has possiblities to scroll towards Opinions via click on button

--- src/lib/components/utils/Pagination.svelte
+++ src/lib/components/utils/Pagination.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
+    import { UpToTop } from "carbon-icons-svelte";
 
     const dsp = createEventDispatcher();
+
+    /** Go to opinions section when click captured in go atop button */
+    function goToOpinion() {
+        document.getElementById("opinions-layout")!.scrollIntoView({
+            behavior: "smooth"
+        })
+    }
 </script>
 
-<div id="set-buttons" class="w-44 flex border border-gray-500 rounded-md h-10 overflow-hidden font-semibold text-zinc-50">
-    <button id="previous" class="w-1/2 text-center bg-zinc-700" on:click={() => dsp("previous")}>
-        Previous
-    </button>
-    <button id="next" class="w-1/2 text-center bg-zinc-700" on:click={() => dsp("next")}>
-        Next
+<div class="flex justify-center items-center relative w-full">
+    <div id="set-buttons" class="w-44 flex border border-gray-500 rounded-md h-10 overflow-hidden font-semibold text-zinc-50">
+        <button id="previous" class="w-1/2 text-center bg-zinc-700" on:click={() => dsp("previous")}>
+            Previous
+        </button>
+        <button id="next" class="w-1/2 text-center bg-zinc-700" on:click={() => dsp("next")}>
+            Next
+        </button>
+    </div>
+    <button class="w-12 h-12 rounded-full flex items-center justify-center absolute right-0 bg-black border border-rose-700 hover:scale-90 scroll-smooth" title="Go atop opinions" on:click={goToOpinion}>
+        <UpToTop size={32} fill="white"/>
     </button>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,13 @@
     import Board from "../lib/components/Board.svelte"
 
     const opinions = [] as Opinions[];
-    /* for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 1000; i++) {
         opinions.push({
             userName: "Jan Kowalski " + i,
             rating: 5.0,
             content: "ABC " + i
         })
-    } */
+    }
 </script>
 
 <Board {opinions} targetSendOpinion={"http://localhost:5173"}/>
