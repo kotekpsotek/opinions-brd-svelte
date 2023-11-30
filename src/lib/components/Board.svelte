68f725c5f2885b7c34ac2433694a34d3782f6523
kotekpsotek <siematumichael@protonmail.com>
2023-11-30 15:23:39 +0100

    Rating-Opinions GUI is set only when are opinions

--- src/lib/components/Board.svelte
+++ src/lib/components/Board.svelte
@@ -3,6 +3,7 @@
     import { Rating as RatingUtils } from "$lib/lib.js";
     import RatingBars from "$lib/components/RatingBars.svelte";
     import OpinionsCmp from "$lib/components/Opinions.svelte";
+    import OpinionsMakeNew from "$lib/components/OpinionsMakeNew.svelte"
     import { setContext } from "svelte";
 
     /** Styling */
@@ -34,6 +35,11 @@
     export let targetSendOpinion: URLTarget | URL;
     export let opinionsCountPerPage = 20;
 
+    // Re-calculate ratings label when opinions amount change
+    $: if (opinions) {
+        ratings = RatingUtils.getRatingsLabelFromOpinions(opinions);
+    }
+
     // Set app context for each other component
     setContext("board", {
         shareOpinionIconSize,
@@ -44,10 +50,21 @@
     });
 </script>
 
-<div id="rating-board" class="flex flex-col w-full p-2 gap-2">
-    <div class="flex justify-center w-full">
-        <Rating total={starsCount} size={styling.starsSizePx} rating={RatingUtils.avgCalculateFromOpinions(opinions)}/>
-    </div>
-    <RatingBars {ratings}/>
-    <OpinionsCmp {opinions}/>
+
+<div id="wrapper-opinions" class="px-5 py-2 w-full h-full flex flex-col gap-y-5">
+    <h1 class="text-3xl font-bold">Rating</h1>
+    {#if opinions.length && ratings?.length}
+        <div id="rating-board" class="flex flex-col w-full p-2 gap-2">
+            <div class="flex justify-center w-full">
+                <Rating total={starsCount} size={styling.starsSizePx} rating={RatingUtils.avgCalculateFromOpinions(opinions)}/>
+            </div>
+            <RatingBars {ratings}/>
+            <OpinionsCmp {opinions}/>
+        </div>
+    {:else}
+        <div id="s">
+            <p  class="text-center text-rose-800">Here isn't any opinions. Let's write new!</p>
+        </div>
+        <OpinionsMakeNew on:opinion-add={({ detail: newOpinion }) => opinions = [newOpinion, ...opinions]}/>
+    {/if}
 </div>
